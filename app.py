6030768be7d7a06074c0c143b48bb4de49bb2490
midupe <miguelpereira2000@gmail.com>
2021-04-26 16:31:00 +0100

    update

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def getpresenca(date):
         return send_file("p/" + date + ".txt", as_attachment=True)
 
 if __name__ == "__main__":
-         app.run(host='0.0.0.0')
+         app.run(host='0.0.0.0', port='80')
